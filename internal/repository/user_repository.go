d13eb9c4ca45fd56a09f54f891b7e8cf52c50908
james <jamessimeao@outlook.com>
2026-02-27 18:23:01 -0300

    Create a method to get a user from its id

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -62,3 +62,32 @@ func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
 
 	return &user, nil
 }
+
+func GetUserById(pool *pgxpool.Pool, id string) (*models.User, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var sql string = `
+		SELECT id, email, password, created_at, updated_at
+		FROM users
+		WHERE id = $1
+	`
+
+	var user models.User
+	var err error
+	err = pool.QueryRow(ctx, sql, id).Scan(
+		&user.Id,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
