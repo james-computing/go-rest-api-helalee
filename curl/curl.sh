00bdfc9db7e8f6611c0a25db71db55853e5468b9
james <jamessimeao@outlook.com>
2026-02-27 12:33:04 -0300

    Add requests

--- curl/curl.sh
+++ curl/curl.sh
@@ -1,1 +1,4 @@
 curl -X POST --json '{"title": "todo title", "completed": false}' http://localhost:8080/todos
+curl -X POST --json '{"title": "another title", "completed": false}' http://localhost:8080/todos
+
+curl -X GET http://localhost:8080/todos
