ea7ef9071396104a36d6a41ecf5a23f251f9baa2
james <jamessimeao@outlook.com>
2026-02-27 19:47:39 -0300

    Turn up into a variable
    The variable should receive up or down as value

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-migrate -path ./migrations -database "postgresql://postgres:replacebypassword@localhost:5432/todo_app_yt?sslmode=disable" up
+# usage: from scripts folder, run
+# ./migrate.sh up
+# or
+# ./migrate.sh down
+migrate -path ../migrations -database "postgresql://postgres:replacebypassword@localhost:5432/todo_app_yt?sslmode=disable" $1
