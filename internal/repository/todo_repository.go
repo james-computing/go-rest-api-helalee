a2ac57c5942e2acf308e4061a642660e3fa7fa03
james <jamessimeao@outlook.com>
2026-02-27 15:09:50 -0300

    Create a method to delete a todo from the database

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"time"
 	"todo_api/internal/models"
 
@@ -143,4 +144,28 @@ func UpdateTodo(pool *pgxpool.Pool, id int, title string, completed bool) (*mode
 	}
 
 	return &todo, nil
+}
+
+func DeleteTodo(pool *pgxpool.Pool, id int) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var sql string = `
+		DELETE
+		FROM todos
+		WHERE id = $1
+	`
+
+	commandTag, err := pool.Exec(ctx, sql, id)
+	if err != nil {
+		return err
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("todo with id %d not found", id)
+	}
+
+	return nil
 }
