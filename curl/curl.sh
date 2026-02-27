e8fa4a13abc7fac81948ce2789df4b6cfea5d4eb
james <jamessimeao@outlook.com>
2026-02-27 15:10:54 -0300

    Create a request to delete a todo

--- curl/curl.sh
+++ curl/curl.sh
@@ -6,3 +6,5 @@ curl -X GET http://localhost:8080/todos
 curl -X GET http://localhost:8080/todos/1
 
 curl -X PUT --json '{"title: "updated title"}' http://localhost:8080/todos/1
+
+curl -X DELETE http://localhost:8080/todos/2
