19d576739a522cc8fa8655ff4ccb90fb47436678
james <jamessimeao@outlook.com>
2026-02-27 17:43:40 -0300

    Correct migration script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-migrate -path ./migrations -database "replaceByConnectionString" up
+migrate -path ./migrations -database "postgresql://postgres:replacebypassword@localhost:5432/todo_app_yt?sslmode=disable" up
