99aba536d34306b9a87959035b77b9f9e6cbfc97
james <jamessimeao@outlook.com>
2026-02-27 19:44:56 -0300

    Add request to register new user

--- curl/curl.sh
+++ curl/curl.sh
@@ -8,3 +8,5 @@ curl -X GET http://localhost:8080/todos/1
 curl -X PUT --json '{"title: "updated title"}' http://localhost:8080/todos/1
 
 curl -X DELETE http://localhost:8080/todos/2
+
+curl -X POST --json '{"email": "john@something.com", "password": "password123"}' http://localhost:8080/auth/register
