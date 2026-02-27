064753e1fded2ab3fdd8b81e3273797070ac1cee
james <jamessimeao@outlook.com>
2026-02-27 13:15:07 -0300

    Add request to get a todo by id

--- curl/curl.sh
+++ curl/curl.sh
@@ -2,3 +2,5 @@ curl -X POST --json '{"title": "todo title", "completed": false}' http://localho
 curl -X POST --json '{"title": "another title", "completed": false}' http://localhost:8080/todos
 
 curl -X GET http://localhost:8080/todos
+
+curl -X GET http://localhost:8080/todos/1
