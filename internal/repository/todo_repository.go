31ce229d75e9cac9c8c2129038bc97da19cebea0
james <jamessimeao@outlook.com>
2026-02-27 13:57:51 -0300

    Create a method to update a todo in the database

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -111,3 +111,36 @@ func GetTodoById(pool *pgxpool.Pool, id int) (*models.Todo, error) {
 
 	return &todo, nil
 }
+
+func UpdateTodo(pool *pgxpool.Pool, id int, title string, completed bool) (*models.Todo, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var sql string = `
+		UPDATE todos
+		SET title = $2,
+			completed = $3,
+			updated_at = CURRENT_TIMESTAMP
+		WHERE id = $1
+		RETURNING id, title, completed, created_at, updated_at
+	`
+
+	var todo models.Todo
+	var err error
+
+	err = pool.QueryRow(ctx, sql, id, title, completed).Scan(
+		&todo.Id,
+		&todo.Title,
+		&todo.Completed,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
